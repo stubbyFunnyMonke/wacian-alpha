98f824c37f232c00ee3315801e00a74b52848a68
FLORENZO RAPHAEL ANTONIO AMOR <2022SHA01012@iacademy.loc>
2022-11-21 12:36:49 +0800

    bug fixes and tweaks

--- scripts/ui/oxygen.gd
+++ scripts/ui/oxygen.gd
@@ -2,8 +2,8 @@ extends ProgressBar
 
 var currentstate = "normal"
 
-func _ready():
-	playerstats.connect("player_stats_changed", self, "_on_stats_changed")
+#func _ready():
+#	playerstats.connect("player_stats_changed", self, "_on_stats_changed")
 
 func _physics_process(_delta):
 	self.value = playerstats.currentOxygen
