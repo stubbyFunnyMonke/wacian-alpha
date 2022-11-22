03c422fd898da9f0ac9cd743d4868a8aa4e1677d
unknown <zpallones@gmail.com>
2022-11-22 21:47:39 +0800

    animations

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -9,6 +9,7 @@ var ROLL_SPEED = 125
 export var FRICTION = 999999999
 
 enum {
+	IDLE,
 	MOVE,
 	ROLL,
 }
@@ -25,6 +26,9 @@ var roll_vector = Vector2.DOWN
 onready var footstep = $footstep
 onready var footstepTimer = $footstep/Timer
 onready var blinkEffects = $BlinkEffects
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 var containermenu
 
@@ -35,9 +39,23 @@ func _ready():
 #	animationTree.active = true
 #	swordHitbox.knockback_vector = roll_vector
 	containermenu = get_tree().get_current_scene().get_node("UI/ContainerMenu")
+# warning-ignore:return_value_discarded
 	playerstats.connect("hit_player", self, "_on_hit")
 
 func _physics_process(delta): #renderstepped :flushed:
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_raw_strength("walkright") - Input.get_action_strength("walkleft")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("walkup")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/idle/blend_position", input_vector)
+		animationTree.set("parameters/walk/blend_position", input_vector)
+		animationState.travel("walk")
+	else:
+		animationState.travel("idle")
+
+		
 	match state:
 		MOVE: move_state(delta)
 		ROLL: roll_state(delta)
@@ -48,7 +66,7 @@ func _physics_process(delta): #renderstepped :flushed:
 	else:
 		collision_layer = 1
 		collision_mask = 1
-	
+		
 	var stunnedAnimation = $stunEffect
 	if playerstats.stunned == true:
 		stunnedAnimation.playing = true
